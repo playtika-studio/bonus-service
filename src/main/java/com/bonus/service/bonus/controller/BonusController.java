ceb4b25c5085cef14d4c7a94e8474b507dd2f485
Razvan Diaconu <ext_razvandi@playtika.com>
2020-04-13 09:58:01 +0300

    MAIN Removed unused logger

--- src/main/java/com/bonus/service/bonus/controller/BonusController.java
+++ src/main/java/com/bonus/service/bonus/controller/BonusController.java
@@ -4,11 +4,8 @@ import com.bonus.service.common.entity.Bonus;
 import com.bonus.service.common.entity.CreateBonusRequest;
 import com.bonus.service.common.exceptions.BonusException;
 import com.bonus.service.bonus.repository.BonusOperations;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -16,8 +13,6 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/bonus")
 public class BonusController {
-
-    private static final Logger log = LoggerFactory.getLogger(BonusController.class);
 
     @Autowired
     BonusOperations bonusOperations;
