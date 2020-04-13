387bc90148d3a89d27b2b9f35cf48baf876149d6
Razvan Diaconu <ext_razvandi@playtika.com>
2020-04-13 23:51:17 +0300

    MAIN Added eureka configuration

--- src/main/java/com/bonus/service/bonus/BonusApplication.java
+++ src/main/java/com/bonus/service/bonus/BonusApplication.java
@@ -2,8 +2,10 @@ package com.bonus.service.bonus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BonusApplication {
 
     public static void main(String[] args) {
